6ffc20027e3984ae7cc727788fabbcb42afe553e
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-08-01 20:52:54 +0200

    update header and homepage-template.php (hero done)

---
+++ wp-content/themes/clinicoeurs/header.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content=""/> <!-- clowns avec -->
+	<meta name="keywords" content="clowns hospitalier"/>
+	<link rel="stylesheet" href="<?= get_stylesheet_directory_uri() . '/public/css/site.css'; ?>"/>
+	<script src="<?= get_stylesheet_directory_uri() . '/public/js/main.js'; ?>" defer></script>
+	<title> <?= __(get_the_title(), 'clinicoeurs'); ?> </title>
+	<link rel="apple-touch-icon" sizes="180x180" href="<?= image(get_field('logo'))['image_url'] ?>">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?= image(get_field('logo'))['image_url'] ?>">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?= image(get_field('logo'))['image_url'] ?>">
+	<link rel="manifest" href="site.webmanifest">
+</head>
+<body>
+<h1 class="sro"><?= __(get_the_title(), 'clinicoeurs'); ?></h1>
+<header class="header">
+	<h2 class="header__title sro"> Menu de navigation principale </h2>
+	<div class="header__container">
+		<a class="header__logo logo" href="<?= home_url() ?>">
+			<div class="logo__container">
+				<img class="logo__image" title="Vers la page d'accueil" src="<?= image(get_field('logo'))['image_url'] ?>" alt="<?= image(get_field('logo'))['alt_text'] ?>">
+				<p class="logo__text"><?= __(get_the_title(), 'clinicoeurs'); ?></p>
+			</div>
+		</a>
+
+
+	</div>
+	<nav class="nav header__container menu menu--desktop display-none">
+		<ul class="nav__list">
+			<?php foreach (clinicoeurs_get_menu('main') as $link): ?>
+				<li class="nav__item">
+					<a href="<?= $link->href; ?>" class="item__link link--menu">
+						<span class="link__label">
+							<?= $link->label; ?>
+						</span>
+					</a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	</nav>
+
+	<div class="header__container menu menu--mobile">
+		<h2 class="sro">Navigation principale</h2>
+		<input type="checkbox" name="menu" id="menu" class="sro menu__checkbox">
+		<label for="menu" class="menu__open ">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu" width="40" height="40" viewBox="0 0 24 24" stroke-width="1">
+				<line x1="4" x2="20" y1="12" y2="12"></line>
+				<line x1="4" x2="20" y1="6" y2="6"></line>
+				<line x1="4" x2="20" y1="18" y2="18"></line>
+			</svg>
+		</label>
+
+		<div class="menu__content">
+			<label for="menu" class="menu__close">
+				<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x">
+					<line x1="18" x2="6" y1="6" y2="18"/>
+					<line x1="6" x2="18" y1="6" y2="18"/>
+				</svg>
+			</label>
+
+			<nav class="menu__nav nav">
+				<?php foreach (clinicoeurs_get_menu('main') as $link): ?>
+					<a href="<?= $link->href; ?>" class="nav__link">
+						<span class="nav__label">
+							<?= $link->label; ?>
+						</span>
+					</a>
+				<?php endforeach; ?>
+			</nav>
+		</div>
+	</div>
+</header>
+<!--	--><?php //dd('') ?>
+
+
+
---
+++ wp-content/themes/clinicoeurs/homepage-template.php
@@ -0,0 +1,222 @@
+<?php /* Template Name: Homepage template */ ?>
+<?php get_header(); ?>
+<?php if (have_posts()): while (have_posts()):the_post(); ?>
+	<main>
+		<div class="hero">
+			<div class="title__container">
+				<img class="hero__shape hero__circle" src="<?= get_site_url() . "/wp-content/uploads/2023/07/Ellipse-12.svg" ?>" alt="">
+				<h2 class="hero__title"> <?= __(get_field('tagline'), 'clinicoeurs') ?> </h2>
+			</div>
+
+			<img class="hero__shape hero__polygon" src="<?= get_site_url() . "/wp-content/uploads/2023/07/Polygon-7.svg" ?>" alt="">
+			<img class="hero__shape hero__star" src="<?= get_site_url() . "/wp-content/uploads/2023/07/Star6.svg" ?>" alt="">
+
+			<img class="hero__shape hero__rectangle" src="<?= get_site_url() . "/wp-content/uploads/2023/07/Rectangle-43.svg" ?>" alt="">
+			<div class="hero__container">
+				<p class="hero__paragraphe"> <?= __(get_field('excerpt'), 'clinicoeurs') ?>  </p>
+				<a class="hero__link" href=""><?= __(get_field('excerpt_link')['link_label'], 'clinicoeurs') ?> </a>
+
+			</div>
+
+		</div>
+
+		<!-- MON FOREACH POUR LA BOUCLE DES NOMS -->
+		<Section class="services">
+			<h2 class="services__title"><?= __(get_field('service_section')['section_title'], 'clinicoeurs') ?></h2>
+			<nav class="services__tabs tabs">
+				<h3 class="sro"> Menu des services </h3>
+				<ul class="tabs__list">
+					<?php foreach (get_field('service_section')['services'] as $services): ?>
+						<li class="tabs__item">
+							<img src="<?= __($services['service']['service_symbol'], 'clinicoeurs') ?>" alt="">
+							<button class="tabs__links">
+								<?= __($services['service']['service_name'], 'clinicoeurs') ?>
+							</button>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+			</nav>
+			<?php foreach (get_field('service_section')['services'] as $services): ?>
+				<?php
+				$image_id = $services['service']['service_image'];
+				$alt_text = get_post_meta($image_id, '_wp_attachment_image_alt', true);
+				$image_url = wp_get_attachment_image_url($image_id, 'full');
+				?>
+
+				<article class="tabs__content content">
+					<img class="content__image" src="<?= $image_url ?>" alt="<?= __($alt_text, 'clinicoeurs') ?>">
+					<h3 class="content__title">  <?= __($services['service']['service_title'], 'clinicoeurs') ?> </h3>
+					<p class="content__paragraph"> <?= __($services['service']['service_text'], 'clinicoeurs') ?> </p>
+					<a class="content__link" href="<?= __($services['service']['service_link'], 'clinicoeurs') ?>" title="<?= __($services['service']['service_link_title'], 'clinicoeurs') ?>">  <?= __($services['service']['service_link_label'], 'clinicoeurs') ?> </a>
+				</article>
+			<?php endforeach; ?>
+		</Section>
+
+		<section class="support">
+			<?php
+			$image_id = get_field('support_section')['section_image'];
+			$alt_text = get_post_meta($image_id, '_wp_attachment_image_alt', true);
+			$image_url = wp_get_attachment_image_url($image_id, 'full');
+			$content = get_field('support_section');
+			?>
+			<h2><?= __($content['section_title'], 'clinicoeurs') ?></h2>
+			<p><?= __($content['section_text'], 'clinicoeurs') ?></p>
+			<img src="<?= $image_url ?>" alt="<?= __($alt_text, 'clinicoeurs') ?>">
+			<a href="<?= $content['section_link'] ?>" title="<?= __($content['section_link_title'], 'clinicoeurs') ?>"> <?= __($content['section_link_label'], 'clinicoeurs') ?> </a>
+		</section>
+
+		<section class="volunteering">
+			<h2 class="volunteering__title"> <?= __(get_field('volunteering_section')['section_title'], 'clinicoeurs') ?>  </h2>
+			<p class="volunteering__text"> <?= __(get_field('volunteering_section')['section_text'], 'clinicoeurs') ?> </p>
+
+			<?php
+			// Faire une requête en DB pour récupérer 3 projets
+			$job = new WP_Query([
+					'post_type' => 'poste',
+					'posts_per_page' => 3,
+			]); ?>
+
+			<?php // Lancer la boucle pour afficher chaque poste
+			if ($job->have_posts()): while ($job->have_posts()):
+				$job->the_post();
+				?>
+				<article class="job__card">
+					<h3 class=""><?= get_the_title(); ?> </h3>
+					<?php
+					$content = get_field('job_presentation');
+					?>
+					<p> <?= __($content['job_location']['label'], 'clinicoeurs') ?> <?= __($content['job_location']['location'], 'clinicoeurs') ?> </p>
+					<p> <?= __($content['job_schedule']['label'], 'clinicoeurs') ?> <?= __($content['job_schedule']['schedule'], 'clinicoeurs') ?> </p>
+					<p> <?= __($content['job_description']['label'], 'clinicoeurs') ?> <?= __($content['job_description']['description'], 'clinicoeurs') ?> </p>
+				</article>
+			<?php endwhile; else: ?>
+				<p> Aucune annonce pour l'instant </p>
+			<?php endif;
+			wp_reset_query(); ?>
+
+			<a href="<?= __(get_field('volunteering_section')['section_link'], 'clinicoeurs') ?>"
+			   title="<?= __(get_field('volunteering_section')['section_link_title'], 'clinicoeurs') ?>">
+				<?= __(get_field('volunteering_section')['section_link_label'], 'clinicoeurs') ?>
+			</a>
+
+			<p class="volunteering__insert">
+				<?= __(get_field('volunteering_section')['volunteering_insert'], 'clinicoeurs') ?>
+			</p>
+		</section>
+
+		<section class="product__section product">
+			<h2 class="product__title">
+				<?= __(get_field('product_section')['section_title'], 'clinicoeurs') ?>
+			</h2>
+			<a href="<?= __(get_field('product_section')['section_link'], 'clinicoeurs') ?>"
+			   title="<?= __(get_field('product_section')['section_link_text'], 'clinicoeurs') ?>">
+				<?= __(get_field('product_section')['section_link_text'], 'clinicoeurs') ?>
+			</a>
+
+
+			<?php
+			// Faire une requête en DB pour récupérer 3 projets
+			$product = new WP_Query([
+					'post_type' => 'produit',
+					'posts_per_page' => 9999,
+			]); ?>
+
+			<?php // Lancer la boucle pour afficher chaque poste
+			if ($product->have_posts()): while ($product->have_posts()):
+				$product->the_post();
+				?>
+
+				<a href="<?= __(get_field('category_link'), 'clinicoeurs') ?>"
+				   title="<?= __(get_field('category_link_title'), 'clinicoeurs') ?>">
+					<article class="product__card">
+						<img src="<?= image(get_field('category_image'))['image_url'] ?>"
+							 alt="<?= __(image(get_field('category_image'))['alt_text'], 'clinicoeurs') ?>">
+						<h3 class=""><?= __(get_the_title(), 'clinicoeurs') ?></h3>
+					</article>
+				</a>
+			<?php endwhile; else: ?>
+				<p> Aucune annonce pour l'instant </p>
+			<?php endif;
+			wp_reset_query(); ?>
+		</section>
+
+		<section class="news news__section">
+			<h2 class="product__title">
+				<?= __(get_field('news_section')['section_title'], 'clinicoeurs') ?>
+			</h2>
+			<?php
+			// Faire une requête en DB pour récupérer 3 projets
+			$recent_post = new WP_Query([
+					'post_type' => 'post',
+					'posts_per_page' => 3,
+			]); ?>
+
+			<?php // Lancer la boucle pour afficher chaque poste
+			if ($recent_post->have_posts()): while ($recent_post->have_posts()):
+				$recent_post->the_post();
+				?>
+				<article class="job__card">
+					<h3>
+						<?= get_the_title() ?>
+					</h3>
+					<img src="<?= image(get_field('article')['article_image'])['image_url'] ?>"
+						 alt="<?= __(image(get_field('article')['article_image'])['alt_text'], 'clinicoeurs') ?>"
+					>
+
+					<p>
+						<?= get_the_date() ?>
+					</p>
+
+					<p>
+						<?= __(get_field('article')['article_summary'], 'clinicoeurs') ?>
+					</p>
+				</article>
+			<?php endwhile; else: ?>
+				<p> Aucune nouvelle pour l'instant </p>
+			<?php endif;
+			wp_reset_query(); ?>
+		</section>
+
+		<section class="testimonials testimonials_section">
+			<h2>
+				<?= __(get_field('testimonials_section')['section_title'], 'clinicoeurs') ?>
+			</h2>
+
+			<?php
+			// Faire une requête en DB pour récupérer 3 projets
+			$testimonials = new WP_Query([
+					'post_type' => 'temoignage',
+					'posts_per_page' => 3,
+			]); ?>
+
+			<?php // Lancer la boucle pour afficher chaque poste
+			if ($testimonials->have_posts()): while ($testimonials->have_posts()):
+				$testimonials->the_post();
+				?>
+				<article class="job__card">
+					<img src="<?= image(get_field('symbol'))['image_url'] ?>"
+						 alt=""
+					>
+					<h3>
+						<?= get_the_title() ?>
+					</h3>
+					<p> <?= get_the_content() ?> </p>
+
+				</article>
+			<?php endwhile; else: ?>
+				<p> Aucune nouvelle pour l'instant </p>
+			<?php endif;
+			wp_reset_query(); ?>
+		</section>
+
+		<section class="sponsor sponsor__section">
+			<h3> <?= get_field('sponsor_section')['section_title'] ?> </h3>
+			<?php foreach (get_field('sponsor_section')['sponsors_images'] as $sponsor): ?>
+				<img src="<?= image($sponsor['sponsor_image'])['image_url'] ?>" alt="<?= __(image($sponsor['sponsor_image'])['alt_text'], 'clinicoeurs') ?>">
+			<?php endforeach; ?>
+		</section>
+
+	</main>
+<?php endwhile; endif; ?>
+<?php get_footer(); ?>
+
