bfc94325be27b47be7fb264f05f154c8efe91a1e
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-08-02 14:09:36 +0200

    add srcset and sizes to img, update volunteering field tag

--- wp-content/themes/clinicoeurs/homepage-template.php
+++ wp-content/themes/clinicoeurs/homepage-template.php
@@ -40,11 +40,12 @@
 				<?php
 				$image_id = $services['service']['service_image'];
 				$alt_text = get_post_meta($image_id, '_wp_attachment_image_alt', true);
-				$image_url = wp_get_attachment_image_url($image_id, 'full');
+				$image_url = wp_get_attachment_image_url($image_id);
+				$srcset = wp_get_attachment_image_srcset($image_id)
 				?>
 
 				<article class="tabs__content content">
-					<img class="content__image" src="<?= $image_url ?>" alt="<?= __($alt_text, 'clinicoeurs') ?>">
+					<img class="content__image" srcset="<?= $srcset ?>" src="<?= $image_url ?>" alt="<?= __($alt_text, 'clinicoeurs') ?>" sizes="(max-width: 767px) 300px, (min-width: 768px) 400px, 400px" >
 					<h3 class="content__title">  <?= __($services['service']['service_title'], 'clinicoeurs') ?> </h3>
 					<p class="content__paragraph"> <?= __($services['service']['service_text'], 'clinicoeurs') ?> </p>
 					<a class="content__link" href="<?= __($services['service']['service_link'], 'clinicoeurs') ?>" title="<?= __($services['service']['service_link_title'], 'clinicoeurs') ?>">  <?= __($services['service']['service_link_label'], 'clinicoeurs') ?> </a>
@@ -56,18 +57,19 @@
 			<?php
 			$image_id = get_field('support_section')['section_image'];
 			$alt_text = get_post_meta($image_id, '_wp_attachment_image_alt', true);
-			$image_url = wp_get_attachment_image_url($image_id, 'full');
+			$image_url = wp_get_attachment_image_url($image_id);
+			$srcset = wp_get_attachment_image_srcset($image_id);
 			$content = get_field('support_section');
 			?>
 			<h2><?= __($content['section_title'], 'clinicoeurs') ?></h2>
 			<p><?= __($content['section_text'], 'clinicoeurs') ?></p>
-			<img src="<?= $image_url ?>" alt="<?= __($alt_text, 'clinicoeurs') ?>">
+			<img srcset="<?= $srcset ?>src="<?= $image_url ?>" alt="<?= __($alt_text, 'clinicoeurs') ?>">
 			<a href="<?= $content['section_link'] ?>" title="<?= __($content['section_link_title'], 'clinicoeurs') ?>"> <?= __($content['section_link_label'], 'clinicoeurs') ?> </a>
 		</section>
 
 		<section class="volunteering">
-			<h2 class="volunteering__title"> <?= __(get_field('volunteering_section')['section_title'], 'clinicoeurs') ?>  </h2>
-			<p class="volunteering__text"> <?= __(get_field('volunteering_section')['section_text'], 'clinicoeurs') ?> </p>
+			<h2 class="volunteering__title"> <?= __(get_field('volunteering_section_1')['section_title'], 'clinicoeurs') ?>  </h2>
+			<p class="volunteering__text"> <?= __(get_field('volunteering_section_1')['section_text'], 'clinicoeurs') ?> </p>
 
 			<?php
 			// Faire une requête en DB pour récupérer 3 projets
@@ -94,13 +96,13 @@
 			<?php endif;
 			wp_reset_query(); ?>
 
-			<a href="<?= __(get_field('volunteering_section')['section_link'], 'clinicoeurs') ?>"
-			   title="<?= __(get_field('volunteering_section')['section_link_title'], 'clinicoeurs') ?>">
-				<?= __(get_field('volunteering_section')['section_link_label'], 'clinicoeurs') ?>
+			<a href="<?= __(get_field('volunteering_section_1')['section_link'], 'clinicoeurs') ?>"
+			   title="<?= __(get_field('volunteering_section_1')['section_link_title'], 'clinicoeurs') ?>">
+				<?= __(get_field('volunteering_section_1')['section_link_label'], 'clinicoeurs') ?>
 			</a>
 
 			<p class="volunteering__insert">
-				<?= __(get_field('volunteering_section')['volunteering_insert'], 'clinicoeurs') ?>
+				<?= __(get_field('volunteering_section_1')['volunteering_insert'], 'clinicoeurs') ?>
 			</p>
 		</section>
 
@@ -129,7 +131,7 @@
 				<a href="<?= __(get_field('category_link'), 'clinicoeurs') ?>"
 				   title="<?= __(get_field('category_link_title'), 'clinicoeurs') ?>">
 					<article class="product__card">
-						<img src="<?= image(get_field('category_image'))['image_url'] ?>"
+						<img srcset="<?= wp_get_attachment_image_srcset(get_field('category_image')) ?>" src="<?= image(get_field('category_image'))['image_url'] ?>"
 							 alt="<?= __(image(get_field('category_image'))['alt_text'], 'clinicoeurs') ?>">
 						<h3 class=""><?= __(get_the_title(), 'clinicoeurs') ?></h3>
 					</article>
@@ -159,9 +161,8 @@
 					<h3>
 						<?= get_the_title() ?>
 					</h3>
-					<img src="<?= image(get_field('article')['article_image'])['image_url'] ?>"
-						 alt="<?= __(image(get_field('article')['article_image'])['alt_text'], 'clinicoeurs') ?>"
-					>
+					<img  srcset="<?= wp_get_attachment_image_srcset(get_field('article')['article_image']) ?>" src="<?= image(get_field('article')['article_image'])['image_url'] ?>"
+						 alt="<?= __(image(get_field('article')['article_image'])['alt_text'], 'clinicoeurs') ?>">
 
 					<p>
 						<?= get_the_date() ?>
@@ -195,8 +196,7 @@
 				?>
 				<article class="job__card">
 					<img src="<?= image(get_field('symbol'))['image_url'] ?>"
-						 alt=""
-					>
+						 alt="">
 					<h3>
 						<?= get_the_title() ?>
 					</h3>
@@ -210,9 +210,10 @@
 		</section>
 
 		<section class="sponsor sponsor__section">
-			<h3> <?= get_field('sponsor_section')['section_title'] ?> </h3>
+			<h2 class="" > <?= get_field('sponsor_section')['section_slogan'] ?> </h2>
+			<!-- N'oublie pas de mettre les alt dans l'admin ET de génerer toutes les tailles d'images -->
 			<?php foreach (get_field('sponsor_section')['sponsors_images'] as $sponsor): ?>
-				<img src="<?= image($sponsor['sponsor_image'])['image_url'] ?>" alt="<?= __(image($sponsor['sponsor_image'])['alt_text'], 'clinicoeurs') ?>">
+				<img srcset=" <?= wp_get_attachment_image_srcset($sponsor['sponsor_image']) ?> " src="<?= wp_get_attachment_url( $sponsor['sponsor_image'] ) ?>" alt="<?= __(get_post_meta( $sponsor['sponsor_image'], '_wp_attachment_image_alt', true ), 'clinicoeurs') ?>">
 			<?php endforeach; ?>
 		</section>
 
