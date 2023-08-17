9f0800804ea5e35feaaab1910d27baa5dea326a8
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-08-17 18:27:51 +0200

    fix tabs object error

--- wp-content/themes/clinicoeurs/resources/ts/main.ts
+++ wp-content/themes/clinicoeurs/resources/ts/main.ts
@@ -1,15 +1,20 @@
-
 import {enableJavaScript} from "./enableJs";
+
 enableJavaScript();
 
 import {displayTheMenuOnDesktop} from "./displayTheMenuOnDesktop";
+
 displayTheMenuOnDesktop.init();
 
-import {tabs} from "./tabs"
-tabs.init();
-
 import {stikyHeader} from "./stikyHeader"
+
 stikyHeader.init();
+
+import {tabs} from "./tabs"
+if (window.location.pathname === '/fr/' || window.location.pathname === '/en/' || window.location.pathname === '/nl/') {
+    tabs.init();
+}
+
 
 
 
